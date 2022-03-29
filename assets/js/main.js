04266d38d13ab655c8ca2d7f026eed3bedb6a1df
robertolatini31 <robertolatini31@gmail.com>
2022-03-29 14:40:50 +0200

    added email control

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creo lista email autorizzate
+
+const email_list = ['fabio@teacher.boolean.com', 'tiziano@tutor.boolean.com', 'fabiola@tutor.boolean.com', 'roberto@student.boolean.com'];
+
+// collego btn_go
+const btnGoElement = document.getElementById('btn_go');
+
+// creo funzione per il click del btn_go
+btnGoElement.addEventListener('click', function() {
+    
+    // chiedo all'utente la sua email
+    const emailUserElement = document.getElementById('email_user').value;
+    console.log(emailUserElement);
+
+    // controlla che sia nella lista di chi può accedere
+    let control = false; // setto a valore di controllo a falso
+    for (let i = 0; i < email_list.length; i++) {
+        const element = email_list[i];
+        if (emailUserElement == element) {
+           control = true; // se è nella lista setto il valore a vero
+        }
+    }
+    // se rientra nella lista stampo il messaggio di bentornato
+    if (control == true) {
+        let h3Element = document.querySelector('.welcome').innerHTML = `Bentornato ${emailUserElement}`;
+    }
+})
+
+
+
