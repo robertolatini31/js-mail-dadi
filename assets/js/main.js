37c6575f70e39bfea495b53e824a7b7e890daf88
robertolatini31 <robertolatini31@gmail.com>
2022-03-29 15:26:19 +0200

    added game

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,49 @@ btnResetElement.addEventListener('click', function() {
     // resetto h3 in html
     let h3Element = document.querySelector('.welcome').innerHTML = ``;
 })
+
+
+// gioco dadi
+
+
+// collego btn_play e btn_stop
+
+const btnPlayElement = document.getElementById('btn_play');
+
+const btnStopElement = document.getElementById('btn_stop');
+
+// avvio al click del btn_play
+btnPlayElement.addEventListener('click', function(){
+
+    // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+
+    let rnd_user = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+    let rnd_computer = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+    console.log(rnd_user);
+    console.log(rnd_computer);
+    // Stabilire il vincitore, in base a chi fa il punteggio più alto
+
+    if (rnd_user > rnd_computer) { // controllo se ha vinto l'utente
+        let winnerElement = document.querySelector('.winner').innerHTML = `Hai Vinto!`;
+    } else if (rnd_user < rnd_computer) { // controllo se ha vinto il computer
+        let winnerElement = document.querySelector('.winner').innerHTML = `Hai Perso!`;
+    } else { // controllo il pareggio
+        let winnerElement = document.querySelector('.winner').innerHTML = `Hai Pareggiato!`;
+    }
+
+    // stampo i risultati
+
+    document.querySelector('.your_result').innerHTML = `Tu hai fatto: ${rnd_user}`;
+    document.querySelector('.computer_result').innerHTML = `La cpu ha fatto: ${rnd_computer}`;
+
+})
+
+// resetto al click del btn_stop
+
+btnStopElement.addEventListener('click', function(){
+
+    let winnerElement = document.querySelector('.winner').innerHTML = ``;
+    document.querySelector('.your_result').innerHTML = ``;
+    document.querySelector('.computer_result').innerHTML = ``;
+
+})
